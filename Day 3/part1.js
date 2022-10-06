2039a14b5637c9eab56312494ce08463295deff0
Adrien French <a_french@ducks.ca>
2022-10-06 12:10:41 -0400

    feat: Day 3 part 1

---
+++ Day 3/part1.js
@@ -0,0 +1,50 @@
+let input = require("./input.json");
+
+//take nth character of each binary num and place in a separate array
+//could use average to see what most common digit is. If avg > 0.5, it is 1
+const matrix = input.map(binaryString => {
+    //turn the binary numbers into a matrix of integers
+    return binaryString.split("").map(digit => parseInt(digit));
+});
+
+/*
+const matrix = [
+    [1,1,0],
+    [0,1,0],
+    [0,1,1]
+]
+*/
+
+//This step isn't strictly necessary since I could just do the calculations without actually transposing the matrix.
+const transposedMatrix = [];
+
+for(let row = 0; row < matrix.length; row++) {
+
+    for(let col = 0; col < matrix[row].length; col++) {
+        if (!transposedMatrix[col]) transposedMatrix.push([]); //create an empty array for this column if it doesn't already exist
+
+        transposedMatrix[col].push(matrix[row][col]);
+    }
+}
+
+const gammaBinary = transposedMatrix.map(row => {
+    //We can calculate the most common value by taking the average and rounding.
+    //If the average is >0.5, then 1 must be more common than 0.
+    //If the average is <0.5, then 0 must be more common than 1.
+    const rowSum = row.reduce((prev, curr) => {
+       return prev + curr; 
+    });
+    return Math.round(rowSum/row.length);
+})
+
+//Invert the gamma to get epsilon
+const epsilonBinary = gammaBinary.map(digit => {
+    return digit === 1 ? 0 : 1;
+});
+
+const gammaDecimal = parseInt(gammaBinary.join(""), 2); //base 2
+const epsilonDecimal = parseInt(epsilonBinary.join(""), 2); //base 2
+
+console.log("Gamma Rate: ", gammaDecimal);
+console.log("Epsilon Rate: ", epsilonDecimal);
+console.log("Gamma * Epsilon = ", gammaDecimal * epsilonDecimal);
